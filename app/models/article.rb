4aa6d714fb7db8b532dafe14f8250b8210488631
Carlos Hurtaado <carloshurtado219@yahoo.com>
2020-01-28 20:17:50 -0800

    Article Table Added to DB

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+
+  # All attributes need to be filled in order to be committed to the DB
+  # other criteria may have to be met
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
