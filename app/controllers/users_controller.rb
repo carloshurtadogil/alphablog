1b3681d65ca5585d9018802f128083c1996fe06c
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-02-07 12:15:37 -0800

    Log in During Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to AlphaBlog, #{@user.username}"
       redirect_to articles_path
     else
