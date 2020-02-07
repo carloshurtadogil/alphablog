9d40adb0dfb39eb94fb4708d4ffbdb5009d59215
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-02-06 21:24:12 -0800

    Create Destroy Session added to Login Form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,24 @@ class SessionsController < ApplicationController
 
   # function to be carried out when logging in
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase) # find the user instance with their email as a parameter
+
+    # if user exists and their password is correct
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      # redirect user to their profile page
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Invalid Credentials'
+      render 'new'
+    end
   end
 
-  # function to remove users
+  # function to remove user from profile and end their session
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to root_path
   end
 end
