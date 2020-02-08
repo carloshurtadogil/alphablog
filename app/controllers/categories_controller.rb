6346e2c00bda41bc0c7ab89d7bf15fc98fd77c79
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-02-07 22:09:42 -0800

    Added Category Controller and Views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,33 @@
 class CategoriesController < ApplicationController
+  # function that carries out operations for the index page
   def index
-    
+    @categories = Category.all
+  end
+
+  # function that carries out operations for the 'New Category' page
+  def new
+    @category = Category.new
+  end
+
+  # function that carries out operations for creating a new category
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:success] = 'Category was created successfully'
+      redirect_to categories_path
+    else 
+      render 'new'
+    end
+  end
+
+  # function that carries out operations for displaying a category
+  def show
+
+  end
+
+  private 
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
